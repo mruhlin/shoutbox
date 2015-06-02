4150bd1e86795f8a115415c8eac89d1d72fa65a4
Mike Ruhlin <mike@ruhl.in>
2015-06-02 11:02:03 -0500

    basic deployment script

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+# start redis
+redis-server &
+
+# deploy comments rest service on port 3000
+cd comments-rest
+rails server -b 0.0.0.0 -p 3000 -e development &
+
+# deploy comments stream service on port 3001 TODO : more than one?
+cd ../comments-stream
+rails server Puma -b 0.0.0.0 -p 3001 -e production &
+
+# deploy photos service on port 3002
+cd ../photo-service
+rails server -b 0.0.0.0 -p 3002 -e development &
+
+# compile jsx etc
+cd ..
+jsx --extension jsx ./ui/src ./ui
+
+# deploy nginx on port 80
+nginx -c nginx.conf -p `pwd`
